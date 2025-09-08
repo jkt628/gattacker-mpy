2b440c60d4688d3a0ba52eb8f9454de6a0a8d6f7
Jack Thomasson <4302889+jkt628@users.noreply.github.com>
2025-09-08 08:50:03 -0400

    manage peripheral name

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import json
 import re
+from typing import Dict
 
 import aioble
+from aioble.central import ScanResult
 from microdot import Microdot
 from microdot.websocket import with_websocket
 from micropython import const
 
 _ADV_TYPE_MANUFACTURER = const(0xFF)
 hexVal = re.compile("0x([0-9A-Fa-f]+)")
+onlyHexName = re.compile(
+    "^[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]$"
+)
 
-peripherals = {}
+
+class Peripheral(ScanResult):
+    def __init__(self, parent):
+        self.device = parent.device
+        self.adv_data = parent.adv_data
+        self.resp_data = parent.resp_data
+        self.rssi = parent.rssi
+        self.connectable = parent.connectable
+        self._name = parent.name()
+        if not self._name:
+            self._name = parent.device.addr_hex().replace(":", "").lower()
+
+    def name(self):
+        return self._name
+
+    def set_name(self, name):
+        if name and self._name != name and onlyHexName.match(self._name):
+            self._name = name
+
+
+peripherals: Dict[str, Peripheral] = {}
 
 
 def PrettyUUID(uuid):
@@ -62,13 +87,18 @@ def web_server():
                             cancel = None
                             break
 
+                        # scan sometimes returns incomplete results, especially result.name().
+                        # the strategy is to build up peripherals and send updates as we get them.
+
                         # convert all the things
-                        address = result.device.addr_hex()
+                        peripheral = Peripheral(result)
+                        address = peripheral.device.addr_hex()
                         peripheralId = address.replace(":", "").lower()
-                        peripherals[peripheralId] = result
-                        name = result.name()
-                        if not name:
-                            name = peripheralId
+                        if peripheralId in peripherals:
+                            peripheral = peripherals[peripheralId]
+                        else:
+                            peripherals[peripheralId] = peripheral
+                        peripheral.set_name(result.name())
                         serviceUuids = [PrettyUUID(x) for x in result.services()]
                         if not serviceUuids:
                             continue
@@ -81,12 +111,12 @@ def web_server():
                             {
                                 "type": "discover",
                                 "peripheralId": peripheralId,
-                                "name": name,
+                                "name": peripheral.name(),
                                 "rssi": result.rssi,
                                 "address": address,
                                 "connectable": result.connectable,
                                 "advertisement": {
-                                    "localName": name,
+                                    "localName": peripheral.name(),
                                     "serviceUuids": serviceUuids,
                                     "manufacturerData": manufacturer,
                                 },
